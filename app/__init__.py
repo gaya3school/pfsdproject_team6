e0415a740e1644b4a4c8ced89873a4be0ff06d98
gayathriabcde <gayathrijabcde@gmail.com>
2026-04-10 13:55:10 +0530

    fixed cors issue

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from core.config import Config
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object(Config)
 
     # Import routes here to avoid circular dependencies
